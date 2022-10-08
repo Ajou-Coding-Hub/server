1c41147df42396813ffc437db4bc5bd674a69c93
gron1gh1 <sgkang@brandazine.com>
2022-10-08 18:15:11 +0900

    feat: init Guide

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { VscodeGateway } from './vscode/vscode.gateway';
+import { GuideModule } from './guide/guide.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, GuideModule],
   controllers: [AppController],
   providers: [AppService, VscodeGateway],
 })
