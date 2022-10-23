f1a5b8ec9636c98edf42bd445259cda5b6027102
gron1gh1 <gron1gh1@gmail.com>
2022-10-24 00:12:25 +0900

    feat: guide init graphql api

---
+++ src/guide/guide.resolvers.ts
@@ -0,0 +1,13 @@
+import { Resolver, Query } from '@nestjs/graphql';
+import { GuideService } from './guide.service';
+import { Guide } from '@prisma/client';
+
+@Resolver('Guide')
+export class GuideResolvers {
+  constructor(private readonly guideService: GuideService) {}
+
+  @Query('guides')
+  async guides(): Promise<Guide[]> {
+    return this.guideService.findAll();
+  }
+}
--- src/guide/guide.service.ts
+++ src/guide/guide.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
-
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Guide } from '@prisma/client';
 @Injectable()
-export class GuideService {}
+export class GuideService {
+  constructor(private prisma: PrismaService) {}
+
+  async findAll(): Promise<Guide[]> {
+    return this.prisma.guide.findMany({});
+  }
+}
