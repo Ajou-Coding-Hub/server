64ce870424e541aff98f65346426513075078922
Jin Hyeok Son <thswlsgur98@gmail.com>
2022-11-21 18:47:30 +0900

    fix migration

--- prisma/migrations/20221121094633_initial_migration/migration.sql
+++ prisma/migrations/20221121094633_initial_migration/migration.sql
@@ -9,6 +9,19 @@ CREATE TABLE `User` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
+CREATE TABLE `Workspace` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+    `ownerId` INTEGER NOT NULL,
+    `status` ENUM('STOPPED', 'CREATING', 'INITIALIZING', 'RUNNING') NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `Workspace_name_key`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
 CREATE TABLE `Project` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `projectName` VARCHAR(191) NOT NULL,
@@ -33,10 +46,21 @@ CREATE TABLE `GuideProblem` (
     `markdown` VARCHAR(1024) NOT NULL,
     `guideId` INTEGER NOT NULL,
     `isSolved` BOOLEAN NOT NULL,
-    `userId` INTEGER NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `_GuideProblemToUser` (
+    `A` INTEGER NOT NULL,
+    `B` INTEGER NOT NULL,
+
+    UNIQUE INDEX `_GuideProblemToUser_AB_unique`(`A`, `B`),
+    INDEX `_GuideProblemToUser_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Workspace` ADD CONSTRAINT `Workspace_ownerId_fkey` FOREIGN KEY (`ownerId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `Project` ADD CONSTRAINT `Project_ownerId_fkey` FOREIGN KEY (`ownerId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -48,4 +72,7 @@ ALTER TABLE `Guide` ADD CONSTRAINT `Guide_ownerId_fkey` FOREIGN KEY (`ownerId`) 
 ALTER TABLE `GuideProblem` ADD CONSTRAINT `GuideProblem_guideId_fkey` FOREIGN KEY (`guideId`) REFERENCES `Guide`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `GuideProblem` ADD CONSTRAINT `GuideProblem_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `_GuideProblemToUser` ADD CONSTRAINT `_GuideProblemToUser_A_fkey` FOREIGN KEY (`A`) REFERENCES `GuideProblem`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_GuideProblemToUser` ADD CONSTRAINT `_GuideProblemToUser_B_fkey` FOREIGN KEY (`B`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
