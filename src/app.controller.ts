6ae68086eb7ecf5a8f72ecc9dd5d4261f6ebaa95
gron1gh1 <gron1gh1@gmail.com>
2022-11-27 02:48:00 +0900

    revert: health check

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,10 +3,7 @@ import { Controller, Get } from '@nestjs/common';
 @Controller()
 export class AppController {
   @Get('')
-  getHealthCheck(): any {
-    return {
-      GITHUB_OAUTH_CLIENT_ID: process.env.GITHUB_OAUTH_CLIENT_ID,
-      GITHUB_OAUTH_CLIENT_SECRET: process.env.GITHUB_OAUTH_CLIENT_SECRET,
-    };
+  getHealthCheck(): string {
+    return 'OK';
   }
 }
